4db36e5192ec825dc87317d374f9b9c230e40fbe
JiHwan Hwang <40712498+neosouler7@users.noreply.github.com>
2020-12-26 22:30:26 +0900

    add ticker
    add ticker

--- manager/utils.py
+++ manager/utils.py
@@ -7,6 +7,7 @@ def read_config():
     with open(os.path.dirname(os.path.realpath(__file__)) + '/../config.json', 'r') as _config_file:
         return json.load(_config_file)
 
+
 def get_current_time(date_format=None, day_delta=None):
     today = datetime.utcnow() + timedelta(hours=9)
 
---
+++ ticker.py
@@ -0,0 +1,41 @@
+import sys
+from datetime import datetime, timedelta
+import time
+
+from data_factory import DataFactory
+from manager.db_manager import DbManager
+
+
+class Ticker:
+    def __init__(self):
+        self.data_factory = DataFactory()
+        self.db_manager = DbManager()
+
+    def run(self, _start_date, _end_date):
+        markets = ["KOSPI", "KOSDAQ"]
+        _start_date = datetime.strptime(_start_date, '%Y%m%d')
+        _end_date = datetime.strptime(_end_date, '%Y%m%d')
+        while True:
+            if _start_date == _end_date:
+                break
+            for market in markets:
+                tickers = self.data_factory.get_ticker_info(market, _start_date.strftime('%Y%m%d'))
+                self.db_manager.insert_ticker(tickers)
+                time.sleep(1)
+            start = start + timedelta(days=1)
+        print(f"[ticker] {_start_date} ~ {_end_date} loaded")
+
+
+if __name__ == "__main__":
+    t = Ticker()
+
+    start_date = sys.argv[1] if len(sys.argv) >= 2 else None
+    end_date = sys.argv[2] if len(sys.argv) >= 3 else None
+    if (start_date and len(start_date) != 8) or (end_date and len(end_date) != 8):
+        print('[WARNING] date SHOULD BE LENGTH OF 8')
+        exit(0)
+    if end_date and int(start_date) >= int(end_date):
+        print('[WARNING] end_date SHOULD BE LATER THAN start_date')
+        exit(0)
+
+    t.run(start_date, end_date)
