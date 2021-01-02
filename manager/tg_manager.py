d0da580d6b65ee5a50cc4970365069c1f5055b5d
JiHwan Hwang <40712498+neosouler7@users.noreply.github.com>
2021-01-02 21:22:56 +0900

    divide msg if too long

--- manager/tg_manager.py
+++ manager/tg_manager.py
@@ -5,6 +5,8 @@ from utils.config import BOT_TOKEN, WARNING_BOT_TOKEN, WATCHDOG_BOT_TOKEN, ADMIN
 
 from telegram.utils.request import Request
 
+MAX_MSG_LENGTH = 4096
+
 
 class TgManager:
     def __init__(self):
@@ -13,7 +15,14 @@ class TgManager:
         self.warning_bot = telegram.Bot(token=WARNING_BOT_TOKEN)
 
     def send_message(self, target, message):
-        self.bot.send_message(target, message, timeout=30, parse_mode=telegram.ParseMode.MARKDOWN_V2, disable_web_page_preview=True)
+        q = len(message) // MAX_MSG_LENGTH
+        if q == 0:
+            self.bot.send_message(target, message, timeout=30, parse_mode=telegram.ParseMode.MARKDOWN_V2, disable_web_page_preview=True)
+            return
+
+        for i in range(q):
+            self.bot.send_message(target, message[MAX_MSG_LENGTH*i:MAX_MSG_LENGTH*(i+1)], timeout=30, parse_mode=telegram.ParseMode.MARKDOWN_V2, disable_web_page_preview=True)
+        self.bot.send_message(target, message[MAX_MSG_LENGTH*q:], timeout=30, parse_mode=telegram.ParseMode.MARKDOWN_V2, disable_web_page_preview=True)
 
     def send_all_message(self, targets, message):
         for target in targets:
